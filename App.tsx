c73a527c3a1a93d3d2c447c9f75c450d0a4b0153
Vineeth pawar R <vineethpawar99@gmail.com>
2023-07-09 08:36:39 +0530

    feat: add navigation setup with keychain service (#2)

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {NativeBaseProvider, Box} from 'native-base';
 import {theme} from './lib/theme';
+import AppNavigator from './lib/navigation/AppNavigator';
 
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
-      <Box flex={1} bg="darkBlue.800" _text={{color: 'white'}}>
-        Hello world
-      </Box>
+      <AppNavigator />
     </NativeBaseProvider>
   );
 }
---
+++ lib/navigation/MainTabNavigator.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainTabNavigator = () => {
+  return <></>;
+};
+
+export default MainTabNavigator;
